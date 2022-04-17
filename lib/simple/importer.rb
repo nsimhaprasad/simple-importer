64730db53c60f0c40257c3a335e431aa40b6855d
Narasimha Prasad <narasimha@beskar.tech>
2022-04-17 15:17:48 +0530

    Initial commit

---
+++ lib/simple/importer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "importer/version"
+
+module Simple
+  module Importer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/simple/importer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Simple::Importer do
+  it "has a version number" do
+    expect(Simple::Importer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
